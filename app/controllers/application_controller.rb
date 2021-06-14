e425d5b2aa87f9bf42941dbfed0c15a62c6b47fd
gilivega <gilibethvega@gmail.com>
2021-06-14 08:01:14 -0400

    se agrega metodo para redirigir a admin a panel al iniciar sesion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
 			redirect_to root_path, notice: "You must be a admin in to access this section"
 		end
 	end
+
+	def after_sign_in_path_for(resource)
+		if current_user.admin?
+			home_admin_path 	
+		else
+			super
+		end
+	end
 end
