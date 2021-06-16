78c764595095160e424ec86258b575e9e80fe44c
gilivega <gilibethvega@gmail.com>
2021-06-16 18:55:09 -0400

    Se revirtieron los cambios

--- app/mailers/my_mailer.rb
+++
@@ -1,8 +0,0 @@
-class MyMailer < ApplicationMailer
-    default :from => "no-reply@bancafriendly.cl"
-      
-    def my_first_email(user)
-        @user = user
-        mail(:to => @user.email, subject: 'Ya eres #FriendlyPro')
-    end
-end
--- config/initializers/sparkpost_rails.rb
+++
@@ -1,3 +0,0 @@
-SparkPostRails.configure do |c|
-    c.api_key = ENV['Sparkpost_API_KEY']
-end
